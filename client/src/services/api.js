54ff34e058c850aaf7d9d8a054d9bc30fcc1b0d1
aas mohd <mraas0003@gmail.com>
2026-09-25 00:10:35 +0530

    added the backend url and changed it from localhost to deployed one

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "/api" });
+const apiUrl = import.meta.env.VITE_API_URL;
+const api = axios.create({ baseURL: apiUrl });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("smartheat_token");
