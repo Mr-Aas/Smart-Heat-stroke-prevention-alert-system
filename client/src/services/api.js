c0ab4e6f4adf1313ad19acb111d17fe338aa8bcb
aas mohd <mraas0003@gmail.com>
2026-09-25 01:28:23 +0530

    fix the frontend to backend connection

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,18 @@
 import axios from "axios";
 
 const apiUrl = import.meta.env.VITE_API_URL;
-const api = axios.create({ baseURL: `${apiUrl}/api` });
+
+const api = axios.create({
+  baseURL: `${apiUrl}/api`
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("smartheat_token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
